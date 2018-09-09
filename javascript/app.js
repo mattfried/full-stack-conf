7d26b733c55a2eb22962a03939f21d347055c104
Matt Fried <mattfried@unknown101f748dc1c5.gateway.2wire.net>
2018-09-09 17:52:04 -0500

    added project files for Full Stack Conference

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+var name = document.getElementById('name');
+var email = document.getElementById('email');
+var ccNum = document.getElementById('cc-num');
+var zipCode = document.getElementById('zip');
+var cvv = document.getElementById('cvv');
+var registerBtn = document.getElementById('registerBtn');
+
+//console.log(ccNum.innerHTML.length);
+
+if (ccNum === 16) {
+  //USE AJAX AND OR CHECK WHEN FORM IS SUBMITTED TO RUN THIS FUNCTION??
+}
+
+ccNum.classList.add('is-invalid');
