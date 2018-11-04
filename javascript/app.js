2baaf456e9c36c1dcc49d738bc497c5fe96c6f27
Matt Fried <mattfried@unknown101f748dc1c5.gateway.2wire.net>
2018-11-03 22:25:36 -0500

    added form validation and interactive schedule

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,43 @@
-var name = document.getElementById('name');
-var email = document.getElementById('email');
-var ccNum = document.getElementById('cc-num');
-var zipCode = document.getElementById('zip');
-var cvv = document.getElementById('cvv');
-var registerBtn = document.getElementById('registerBtn');
+/*** Disabling form submission if there are invalid fields ***/
 
-//console.log(ccNum.innerHTML.length);
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
 
-if (ccNum === 16) {
-  //USE AJAX AND OR CHECK WHEN FORM IS SUBMITTED TO RUN THIS FUNCTION??
+
+
+/*** Highlight current schedule item based on current time ***/
+
+var scheds = document.getElementsByClassName('sched');
+var d = new Date();
+var t = d.getHours();
+
+if (t == 9) {
+  scheds[0].classList.add('list-group-item-success');
+} else if (t == 10) {
+  scheds[1].classList.add('list-group-item-success');
+} else if (t == 11) {
+  scheds[2].classList.add('list-group-item-success');
+} else if (t == 12) {
+  scheds[3].classList.add('list-group-item-success');
+} else if (t == 13) {
+  scheds[4].classList.add('list-group-item-success');
+} else if (t == 14) {
+  scheds[5].classList.add('list-group-item-success');
+} else if (t == 15) {
+  scheds[6].classList.add('list-group-item-success');
 }
-
-ccNum.classList.add('is-invalid');
